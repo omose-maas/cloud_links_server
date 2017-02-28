b6b67681e087c649e8348487a8d35bda9089ac56
omose-maas <hiro.tx@kmd.keio.ac.jp>
2017-02-28 19:14:40 +0900

    update server

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 #setup env
 #pip install flask-socketio pyjsonrpc
 
-from flask import Flask, render_template, session
+from flask import Flask, render_template, request, session
 from flask_socketio import SocketIO, send, emit, disconnect
 import json
 import pyjsonrpc
@@ -13,12 +13,23 @@ import send_socketio
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+count = 0
 
 @app.route('/')
 def index():
-	request_json = pyjsonrpc.create_request_json("BasicCommunication.get_status")
-	send_socketio.send_data(socketio,request_json)
 	return render_template('index.html')
+
+@app.route('/post', methods = ['POST'])
+def hello():
+	if request.method == 'POST':
+		if request.form['action'] == 'post':
+			data = {'latitude' : request.form['latitude'], 'longitude' : request.form['longitude']}
+			request_json = pyjsonrpc.create_request_json("DriverlessCar.set_destination",data)
+			send_socketio.send_data(socketio,request_json)
+		#test
+		global count
+		count = count + 1
+	return render_template('index.html', message = count)
 
 @socketio.on('my_response')
 def print_response_message(response):
